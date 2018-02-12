8cbb8ee0ed155b38dfcc44ae7acaf4b9b9fd38ba
Nathan Marciano <marcianonathan1@gmail.com>
2018-02-12 16:49:04 +0100

    log to chat

--- src/client/js/chat.js
+++ src/client/js/chat.js
@@ -1,54 +1,53 @@
 class Chat {
   constructor () {
     const io = require('socket.io-client');
-    this.socket = io.connect('http://localhost:8082');
-    this.zone_chat = document.querySelector('#zone_chat');
-    this.inputSendMessage = document.querySelector('.input--send-message');
-  }
 
+    this.socket = io.connect('http://localhost:8082');
+    this.zoneChat = document.querySelector('#zone_chat');
+    this.inputSendMessage = document.querySelector('.input--send-message');
+    this.inputPseudo = document.querySelector('.input--pseudo');
+  }
+  init () {
+    this.pseudo = '';
+    this.socket.on('message', (data) => {
+      this.insereMessage(data.pseudo, data.message);
+    });
+  }
+  setPseudo () {
+    this.inputPseudo.addEventListener('keypress', (e) => {
+      var key = e.keyCode;
+
+      if (key === 13) {
+        this.pseudo = this.inputPseudo.value;
+        this.inputPseudo.disabled = true;
+      }
+    });
+  }
   sendMessage () {
     this.inputSendMessage.addEventListener('keypress', (e) => {
       var key = e.keyCode;
+
       if (key === 13) {
         this.insereMessage(this.pseudo, this.inputSendMessage.value);
+        this.socket.emit('nouveau_client', this.pseudo);
         this.socket.emit('message', this.inputSendMessage.value);
         this.inputSendMessage.value = '';
       }
     });
   }
-
-  getInputMessage () {
-    return document.querySelector('.input--send-message').value;
-  }
   insereMessage (pseudo, message) {
     const p = document.createElement('p');
 
-    p.textContent = this.pseudo + ' : ' + this.inputSendMessage.value;
-    this.zone_chat.appendChild(p);
+    p.textContent = pseudo + ' : ' + message;
+    this.zoneChat.prepend(p);
     return false;
   }
-
-  init () {
-    this.pseudo = prompt('Quel est votre pseudo ?');
-    document.title = this.pseudo;
-    this.socket.on('message', (data) => {
-        this.insereMessage('',data.message);
-    })
-
-
-  }
-
   run () {
     this.init();
+    this.setPseudo();
     this.sendMessage();
-  } 
-
-
-
-
-
+  }
 }
 const chat = new Chat();
+
 chat.run();
-
-
--- src/server/app.js
+++ src/server/app.js
@@ -1,27 +1,26 @@
 const express = require('express');
-let app = require('express')();
+let app = express();
 let server = require('http').createServer(app);
 let io = require('socket.io').listen(server);
-let ent = require('ent'); // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
-let fs = require('fs');
+let ent = require('ent');
+const path = require('path'); // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
 // Chargement de la page index.html
 
-app.use(express.static(__dirname + '/../client'));
+//app.use(express.static(__dirname + '/../client'));
+app.use (express.static(path.join(`${__dirname}/../client`)));
 
-io.sockets.on('connection', (socket, pseudo) => {
+io.sockets.on('connection', socket => {
   // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-  socket.on('nouveau_client', (pseudo) => {
+  socket.on('nouveau_client', pseudo => {
     pseudo = ent.encode(pseudo);
     socket.pseudo = pseudo;
     socket.broadcast.emit('nouveau_client', pseudo);
   });
 
   // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
-  socket.on('message', (message) => {
-    console.log(message);
+  socket.on('message', message => {
     message = ent.encode(message);
     socket.broadcast.emit('message', {'pseudo': socket.pseudo, 'message': message});
-    console.log(message);
   });
 });
 
